5a613e552fd3bcdaf7803b21b17e512d18597a02
Miguelvitalr <m.vitalrg@gmail.com>
2024-09-08 19:06:03 -0300

    Game

--- MicroJm/Assets/Scripts/PlayerMovement.cs
+++ MicroJm/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody2D rb;
-    public Vector2 moveDirection;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 moveDirection; 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     public void Update()
     {
@@ -25,9 +28,20 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector2(moveX, moveY).normalized;
+        
+
     }
     public void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        anim.SetFloat("Horizontal", moveDirection.x);
+        anim.SetFloat("Vertical", moveDirection.y);
+
+        if(moveDirection != Vector2.zero)
+        {
+            anim.SetFloat("LastHorizontal", moveDirection.x);
+            anim.SetFloat("LastVertical", moveDirection.y);
+        }
     }
 }
